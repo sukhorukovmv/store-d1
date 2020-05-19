cb560592ae89b4b228b35873a4f29d554132da4f
sukhorukov.mv <sukhorukov.mv@reliab.tech>
2020-05-19 10:00:37 +0300

    added: adminPage

---
+++ src/RouterAdmin.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import ProductList from "./components/ProductList";
+import Auth from "./components/Auth/Auth";
+import AdminPage from "./components/Admin/AdminPage";
+import { ProductConsumer } from "./context";
+
+export default function RouterAdmin() {
+  return (
+    <ProductConsumer>
+      {(value) => {
+        const  {isAdmin}  = value;
+        console.log(isAdmin);
+        if (isAdmin) {
+          return <AdminPage />
+        } else {
+          return (
+            <React.Fragment>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={ProductList} />
+                <Route path="/details" component={Details} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/auth" component={Auth} />
+                <Route component={Default} />
+              </Switch>
+            </React.Fragment>
+          );
+        }
+      }}
+    </ProductConsumer>
+  );
+}
