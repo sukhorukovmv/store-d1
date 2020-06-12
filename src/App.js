4aa270fd6b95a74a948e41c20ca9b6b74515e727
sukhorukovmv <sukhorukov.maksim95@gmail.com>
2020-06-12 17:36:27 +0300

    delete test functional

--- src/App.js
+++ src/App.js
@@ -1,91 +1,30 @@
 import React from "react";
 import { Switch, Route, Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import ProductList from "./components/ProductList";
+import Auth from "./components/Auth/Auth";
+import Test from "./components/Redux/Test";
+import Modal from "./components/Modal";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Modal from "./components/Modal";
-import RouterAdmin from "./RouterAdmin";
-
-import { createStore, bindActionCreators } from "redux";
-import { connect } from "react-redux";
-
-const initialState = {
-  firstName: "Oleg",
-  secondName: "Pavlov",
-};
-
-const ACTION_CHANGE_FIRST_NAME = "ACTION_CHANGE_FIRST_NAME";
-const ACTION_CHANGE_SECOND_NAME = "ACTION_CHANGE_SECOND_NAME";
-
-const changeFirstName = (newFirstName) => {
-  return {
-    type: ACTION_CHANGE_FIRST_NAME,
-    payload: newFirstName,
-  };
-};
-
-const changeSecondName = (newSecondName) => {
-  return {
-    type: ACTION_CHANGE_SECOND_NAME,
-    payload: newSecondName,
-  };
-};
-
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ACTION_CHANGE_FIRST_NAME:
-      return { ...state, firstName: action.payload };
-    case ACTION_CHANGE_SECOND_NAME:
-      return { ...state, secondName: action.payload };
-  }
-  return state;
-};
-
-export const store = createStore(rootReducer);
-
-export const putStateToProps = (state) => {
-  return {
-    firstName: state.firstName,
-    secondName: state.secondName,
-  };
-};
-
-export const putActionsToProps = (dispatch) => {
-  return {
-    changeFirstName: bindActionCreators(changeFirstName, dispatch),
-    changeSecondName: bindActionCreators(changeSecondName, dispatch),
-  };
-};
 
 export default function App(props) {
-  const { firstName, secondName, changeFirstName, changeSecondName } = props;
   return (
     <React.Fragment>
-      <RouterAdmin />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/test" component={Test} />
+        <Route component={Default} />
+      </Switch>
       <Modal />
-      <div>
-        <div>
-          <input
-            type="text"
-            value={firstName}
-            placeholder="First name"
-            onChange={(event) => {
-              changeFirstName(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={secondName}
-            placeholder="Second name"
-            onChange={(event) => {
-              changeSecondName(event.target.value);
-            }}
-          />
-        </div>
-        <div>{firstName + " " + secondName}</div>
-      </div>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./context";
-import { connect, Provider } from "react-redux";
-import { store, putStateToProps, putActionsToProps } from "./App";
 import * as serviceWorker from "./serviceWorker";
 import {} from "./components/Redux/Test";
-const WrapperMainComponent = connect(putStateToProps, putActionsToProps)(App);
 
 ReactDOM.render(
   <React.StrictMode>
     <ProductProvider>
-      <Provider store={store}>
         <Router>
-          <WrapperMainComponent />
+          <App />
         </Router>
-      </Provider>
     </ProductProvider>
   </React.StrictMode>,
   document.getElementById("root")
@@ -27,26 +22,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-const stt = {
-  name: "jvm.memory.used",
-  description: "The amount of used memory",
-  baseUnit: "bytes",
-  measurements: [{ statistic: "VALUE", value: 1.81920296e8 }],
-  availableTags: [
-    { tag: "area", values: ["heap", "nonheap"] },
-    {
-      tag: "id",
-      values: [
-        "CodeHeap 'profiled nmethods'",
-        "Eden Space",
-        "CodeHeap 'non-profiled nmethods'",
-        "Survivor Space",
-        "Compressed Class Space",
-        "Metaspace",
-        "CodeHeap 'non-nmethods'",
-        "Tenured Gen",
-      ],
-    },
-  ],
-};
